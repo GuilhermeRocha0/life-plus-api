a950cabb6cac66a499c01b7dd58c8186f50da370
Guilherme Oliveira Rocha <gui.rocha009@gmail.com>
2026-05-07 01:35:34 -0300

    fix: configure express api for vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import path from 'path'
 import { setupSwagger } from './swagger'
+
 import userRoutes from './routes/userRoutes'
 import authRoutes from './routes/authRoutes'
 import examRoutes from './routes/examRoutes'
@@ -12,6 +12,7 @@ import medicineHistoryRoutes from './routes/medicineHistoryRoutes'
 dotenv.config()
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
@@ -23,5 +24,4 @@ app.use('/medicines', medicineHistoryRoutes)
 
 setupSwagger(app)
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+export default app
